c32f72491074e86c2252233005200b05b38c6ee6
john.curtis <john.curtis@saifeinc.com>
2015-09-15 11:38:40 -0700

    moved some files that ended up in the wrong place.

--- SAIFEDARDemo/java/run.sh
+++ SAIFEDARDemo/java/run.sh
@@ -1,8 +1,18 @@
-#java -cp bin:java-lib-2.1.0-dev-63.jar:gson-2.2.2.jar:guava-11.0.jar -Djava.library.path=./lib/ com.saife.demo.saifeVol -fsample.txt -s
+#!/bin/bash
+set -x 
 if [[ $# -eq 0 ]]; then
-	echo "use -f<file_name> to retrieve a file."
-	echo "use -s -f<file_name> to store a file."
+	echo "use -f <stored_file_name> -o <output_file_name> to retrieve a file."
+	echo "use -s -f <input_file_name> to store a file. Input file must be in current directory."
+	echo "use -r to remove and recreate volume."
 	exit 0
 fi
-echo "java -cp bin:lib/java-lib-2.1.0-dev-63.jar:lib/gson-2.2.2.jar:lib/guava-11.0.jar -Djava.library.path=./lib/SAIFE com.saife.demo.saifeVol ${*}"
-java -cp bin:lib/java-lib-2.1.0-dev-63.jar:lib/gson-2.2.2.jar:lib/guava-11.0.jar -Djava.library.path=./lib/SAIFE com.saife.demo.saifeVol ${*}
+
+export depends="lib/java-lib-2.1.0-dev-63.jar lib/gson-2.2.2.jar lib/guava-11.0.jar lib/SAIFE/libsaife.so lib/SAIFE/libCecCryptoEngine.so lib/SAIFE/libroxml.so"
+for lib in $depends; do
+	if [[ ! -e $lib ]]; then
+		echo "ERROR: missing $lib"
+		exit 0
+	fi
+done
+
+java -cp bin:lib/java-lib-2.1.0-dev-63.jar:lib/gson-2.2.2.jar:lib/guava-11.0.jar -Djava.library.path=./lib/SAIFE com.saife.demo.SaifeVol ${*}
