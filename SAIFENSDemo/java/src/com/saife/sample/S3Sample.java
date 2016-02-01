be35a566d9c88fe10b4ddd391899d47be04dcf9b
Jacob Gersztyn <jabocg@gmail.com>
2016-02-01 09:02:43 -0700

    Update copyright

--- SAIFENSDemo/java/src/com/saife/sample/S3Sample.java
+++ SAIFENSDemo/java/src/com/saife/sample/S3Sample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 SAIFE Inc.
+ * Copyright (c) 2016 SAIFE Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
  *
  *
  */
-
 package com.saife.sample;
 
 import java.io.File;
--- SAIFENSDemo/java/src/com/saife/sample/SaifeManager.java
+++ SAIFENSDemo/java/src/com/saife/sample/SaifeManager.java
@@ -1,4 +1,5 @@
-/* Copyright (c) 2015 SAIFE Inc.
+/* 
+ * Copyright (c) 2016 SAIFE Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
