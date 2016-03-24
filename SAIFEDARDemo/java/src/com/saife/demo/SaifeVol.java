6e0404bb74024c1d32aac4250789d922cec8f862
Jacob Gersztyn <jabocg@gmail.com>
2016-03-24 16:08:39 -0700

    Add entropy

--- SAIFEDARDemo/java/src/com/saife/demo/SaifeVol.java
+++ SAIFEDARDemo/java/src/com/saife/demo/SaifeVol.java
@@ -2,6 +2,7 @@ package com.saife.demo;
 
 import java.io.FileReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,6 +12,7 @@ import java.util.List;
 import com.google.common.io.ByteStreams;
 import com.google.gson.*;
 
+import com.saife.InsufficientEntropyException;
 import com.saife.Saife;
 import com.saife.Address;
 import com.saife.SaifeFactory;
@@ -39,7 +41,7 @@ public class SaifeVol implements Runnable {
   /** The default path where all persisted SAIFE data is written. */
   static final String defaultKeyStore = ".SaifeStore";
 
-  /**gti 
+  /** 
    * The default password to unlock the SAIFE private key. In practice a user is always prompted for this input.
    */
   static final String defaultPassword = "mysecret";
@@ -96,7 +98,49 @@ public class SaifeVol implements Runnable {
         final List<Address> addressList = new ArrayList<Address>();
 
         // Generate the public/private key pair and certificate signing request.
-        final CertificationSigningRequest csr = saife.generateSmCsr(dn, defaultPassword, addressList);
+
+        // add required entropy
+        boolean entropic = false;
+
+        CertificationSigningRequest csr = null;
+
+        // getting entropy from /dev/urandom
+        final FileInputStream fin = new FileInputStream("/dev/urandom");
+
+        byte[] b;
+
+        while (!entropic) {
+            try {
+                // read entropy
+                b = new byte[32];
+                fin.read(b);
+
+                // add entropy to SAIFE library, assuming 4 of 8 bits are
+                // entropic enough
+                System.out.println("Adding entropy to SAIFE library");
+                saife.AddEntropy(b, 4);
+
+                // attempt to create the certificate singing request
+                csr = saife.generateSmCsr(dn, defaultPassword, addressList);
+
+                // if success, we are entropic enough
+                entropic = true;
+                
+            } catch (final InsufficientEntropyException iee) {
+                System.out.println(iee.getMessage());
+                entropic = false;
+            } catch (final IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+        try { 
+            fin.close();
+        } catch (final IOException ioe) {}
+
+        if (null == csr) {
+            System.exit(1);
+        }
 
         // Add additional capabilities to the SAIFE capabilities list that convey the application specific capabilities.
         final List<String> capabilities = csr.getCapabilities();
